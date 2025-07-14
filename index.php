019d53581f092f5d27b25f5ed7a4f59f120b0d3e
notfakeluccas <contatolucasafs@gmsil.com>
2025-07-14 17:50:42 -0300

    server

--- index.php
+++ index.php
@@ -81,7 +81,7 @@
             font-weight: 500;
             transition: box-shadow 0.2s;
             box-sizing: border-box;
-            margin-top: 190px; 
+            margin-top: 200px; 
         }
 
         .security-link:hover {
