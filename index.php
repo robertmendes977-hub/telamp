b192714c533d242f03d0b023b2409646c547b35a
notfakeluccas <contatolucasafs@gmsil.com>
2025-07-14 17:19:56 -0300

    server

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
 
         /* Título principal (coluna da esquerda) */
         .main-title {
-            font-size: 32px;
+            font-size: 20px;
             /* ALTERAÇÃO: Aplicando o negrito */
             font-weight: 600; /* Ou 'bold' */
             color: #333;
