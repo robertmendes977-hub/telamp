f336fa20777cafce241f1529d30a4da680bb52f0
notfakeluccas <contatolucasafs@gmsil.com>
2025-07-14 18:11:34 -0300

    server

---
+++ senha.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Digite sua senha</title>
+    <style>
+        /* Reset básico */
+        body {
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; 
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+            background-color: #f5f5f5; 
+        }
+
+        /* Estilo do cabeçalho principal */
+        .main-header {
+            background-color: #ffe600;
+            padding: 0 680px;
+            box-shadow: 0 1px 2px 0 rgba(0,0,0,.1);
+            height: 60px;
+            display: flex;
+            align-items: center;
+            justify-content: flex-start;
+        }
+
+        .main-header img {
+            height: 37px;
+            vertical-align: middle;
+        }
+    </style>
+</head>
+<body>
+
+    <header class="main-header">
+        <img src="https://http2.mlstatic.com/storage/mobile-on-demand-resources/image/web-private-nav-mp-logo_1X?updatedAt=1746639317789" alt="Mercado Pago">
+    </header>
+
+    </body>
+</html>
